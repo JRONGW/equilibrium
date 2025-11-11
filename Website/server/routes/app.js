e5c3bd5624cea279ebed64fc2f3f03b89cbd79bd
Junrong Wang <jrongwchn@gmail.com>
2025-11-11 11:37:42 +0000

    update

--- Website/server/routes/app.js
+++ Website/server/routes/app.js
@@ -9,6 +9,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/js', express.static(path.join(process.cwd(), 'Website', 'client', 'js')));
+app.use('/css', express.static(path.join(process.cwd(), 'Website', 'client', 'css')));
+app.use('/img', express.static(path.join(process.cwd(), 'Website', 'client', 'img')));
+
+app.get('/', (_, res) =>
+  res.sendFile(path.join(process.cwd(), 'Website', 'client', 'index.html'))
+);
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
